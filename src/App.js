22d8d4c563f3794d5acd4c91406d697ced79b538
Ruma303 <matteoh8@gmail.com>
2023-07-14 12:53:42 +0200

    bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import ClassComponents from './Components/ClassComponent';
 import EventComponent from './Components/EventComponent';
+import Bootstrap from './Components/Bootstrap';
 
 function App() {
   return (
     <div className="App">
        <ClassComponents />
        <EventComponent />
+       <Bootstrap />
     </div>
   );
 }
---
+++ src/Components/Bootstrap.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Bootstrap = () => {
+    return (
+        <div className="container">
+            <h1 className="text-center fs-1 text-primary">
+                Bootstrap in React!</h1>
+        </div>
+    )
+}
+
+export default Bootstrap;
