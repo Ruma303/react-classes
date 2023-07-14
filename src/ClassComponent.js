7770e31d9f43c0ac9b407ceb132da52e8158c211
Ruma303 <matteoh8@gmail.com>
2023-07-14 09:49:34 +0200

    class components

---
+++ src/ClassComponent.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+class ClassComponents extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            counter: 0,
+            message: "ClassComponents",
+        };
+        console.log("constructor");
+    }
+    //$ Metodi di Montaggio
+    static getDerivedStateFromProps(props, state) {
+        console.log("getDerivedStateFromProps");
+        return null;
+    }
+    componentDidMount() {
+        console.log("Componente montato");
+    }
+
+    //$ Metodi di Aggiornamento
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log("shouldComponentUpdate");
+        return true;
+    }
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        console.log("getSnapshotBeforeUpdate");
+        return "snapshot";
+    }
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log("Componente aggiornato", snapshot);
+    }
+
+    //$ Metodo di Smontaggio
+    componentWillUnmount() {
+        console.log("Componente in procinto di essere smontato");
+    }
+    render() {
+        console.log("render");
+        return (
+            <div>
+                <h1>{this.state.message}</h1>
+                <button
+                    onClick={() => this.setState({ counter: this.state.counter + 1 })}
+                >
+                    Aumenta contatore
+                </button>
+                <p>{this.state.counter}</p>
+                <div className="class-components"></div>
+            </div>
+        );
+    }
+}
+export default ClassComponents;
